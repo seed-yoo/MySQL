0e77a5857c2ffed599280b0eb294c62798aad645
seed-yoo <lukdown10@naver.com>
2024-01-23 18:41:04 +0900

    sql 수업 3일차 및 연습문제 (완료)

--- Ex/hr.sql
+++ Ex/hr.sql
@@ -654,17 +654,247 @@ select first_name, ifnull(commission_pct, '0')
 from employees;
 
 
+-- ---------------------------------------------------
+-- 수업 3일차
+-- ---------------------------------------------------
+-- 그룹함수
+
+-- 단일형함수
+select first_name,
+	   salary,
+       round(salary, 2)
+from employees;
+-- 오류가 난다
+select first_name,
+	   salary,
+       sum(salary)
+from employees;
+
+-- 그룹형함수
+select sum(salary)
+from employees;
+
+-- -------------------------------------------
+# count()
+-- -------------------------------------------
+select *
+from employees;
+
+select count(*)
+from employees;
+
+select count(first_name)
+from employees;
+
+select count(manager_id)
+from employees;
+
+select count(commission_pct)
+from employees;
+
+select count(*), count(commission_pct)
+from employees;
+
+-- 월급이 16000 초과 직원의 수는?
+select count(*)
+from employees
+where salary > 16000;
+
+-- -------------------------------------------
+# sum()
+-- -------------------------------------------
+select count(*), 
+	   sum(salary),
+       sum(employee_id)
+from employees;
+
+-- -------------------------------------------
+# avg()		null 포함여부 주의
+-- -------------------------------------------
+select count(*), sum(salary), avg(salary)
+from employees;
+
+select count(*), sum(salary), avg(ifnull(salary, 0))
+from employees;
+
+-- -------------------------------------------
+# max()/min()
+-- -------------------------------------------
+select count(*),
+	   max(salary),
+	   min(salary)
+from employees;
+
+-- -------------------------------------------
+#  GROUP BY 절
+-- -------------------------------------------
+select department_id, salary
+from employees;
+
+select department_id as '부서번호',
+	   sum(salary) as '그룹별월급합',
+       max(salary) as '그룹별 최대 월급' ,
+       avg(salary) as '그룹별평균',
+       count(*) as '그룹별 부서갯수'
+from employees
+group by department_id;
 
 
+select department_id,
+	   job_id,
+       count(*),
+       sum(salary)
+from employees
+group by department_id, job_id;
 
 
+-- 월급(salary)의 합계가 20000 이상인 부서의 부서 번호와 , 인원수, 월급합계를 출력하세요
+select department_id,
+	   count(*),
+       sum(salary)
+from employees
+-- where department_id = 100			※ 사용o
+-- where sum(salary) >= 20000		※ 사용x where절에는 그룹함수를 쓸수 없다.
+group by department_id
+;
 
 
+-- -------------------------------------------
+# HAVING 절
+-- -------------------------------------------
+-- 월급(salary)의 합계가 20000 이상인 부서의 부서 번호와 , 인원수, 월급합계를 출력하세요
+select department_id,
+	   count(*),
+       sum(salary)
+from employees
+-- where department_id = 100	--	※ 사용o
+-- where sum(salary) >= 20000		※ 사용x
+group by department_id
+having sum(salary) >= 20000;
+-- and department_id = 100;
+
+-- -------------------------------------------
+# IF ~ ELSE 문/CASE ~ END 문
+-- -------------------------------------------
+select first_name,
+	   salary,
+       commission_pct,
+	   if(commission_pct is null, 0, 1) as state
+from employees;
+
+select commission_pct,
+	   ifnull(commission_pct, 0)
+from employees;
+
+-- 직원아이디, 월급, 업무아이디, 실제월급(realSalary)을 출력하세요.
+-- 실제월급은 job_id 가 'AC_ACCOUNT' 면 월급+월급*0.1,
+-- 'SA_REP' 월급+월급*0.2,
+-- 'ST_CLERL' 면 월급+월급*0.3
+-- 그외에는 월급으로 계산하세요
+select employee_id,
+	   salary,
+       job_id,
+       case when job_id = 'AC_ACCOUNT' then salary*salary*0.1
+			when job_id = 'SA_REP' then salary*salary*0.2
+			when job_id = 'ST_CLERL' then salary*salary*0.3
+            else salary
+	   END realSalary
+from employees;
+
+-- 직원의 이름, 부서번호, 팀을 출력하세요
+-- 팀은 코드로 결정하며 부서코드가
+-- 10~50 이면 'A-TEAM'
+-- 60~100이면 'B-TEAM'
+-- 110~150이면 'C-TEAM'
+-- 나머지는 '팀없음' 으로
+-- 출력하세요
+select concat(first_name, '-', last_name) as '이름',
+	   department_id as '부서번호',
+       case when department_id between 10 and 50 then 'A-TEAM'
+			when department_id >= 60 and department_id <= 100 then 'B-TEAM'
+            when department_id >= 110 and department_id <= 150 then 'C-TEAM'
+			else '팀없음'
+       END 'TEAM'
+from employees
+;
+
+-- -------------------------------------------
+# join
+-- -------------------------------------------
+-- 사원이름, 부서번호
+select first_name, department_id
+from employees;
+
+-- 직원
+select *
+from employees;
+
+-- 부서
+select *
+from departments;
+
+-- 테이블 별명쓰기
+-- 겹치지 않는 컬럼명은 테이블 별명을 쓰지 않아도 된다
+-- 하지만 써도 된다
+-- 겹치는 컬럼명은 꼭 써야 된다
+select e.first_name,
+	   d.department_name,
+       e.department_id,
+       d.department_id
+from employees e, departments d;
 
 
+select first_name,
+       department_name,
+       e.department_id,
+       d.department_id
+from employees e, departments d
+where e.department_id = d.department_id;
+
+select e.first_name,
+	   d.department_name,
+       e.department_id,
+       d.department_id
+from employees e
+inner join departments d
+	on e.department_id = d.department_id
+;
+
+-- 모든 직원이름, 부서이름, 업무명 을 출력하세요 *3개의 테이블
+select e.first_name,
+	   d.department_name,
+       j.job_title,
+       j.job_id
+from employees e, departments d, jobs j
+where e.department_id = d.department_id 
+and e.job_id = j.job_id;
 
 
+select e.first_name,
+	   d.department_name,
+       j.job_title,
+       j.job_id
+from employees e
+inner join departments d 
+	on e.department_id = d.department_id
+inner join jobs j 
+	on e.job_id = j.job_id;
 
+-- 이름, 부서번호, 부서명, 업무아이디, 업무명, 도시아이디, 도시명
+select first_name,
+	   d.department_id,
+       d.department_name,
+       j.job_id,
+       j.job_title,
+       l.location_id,
+       l.city
+from employees e
+inner join departments d
+	on e.department_id = d.department_id
+inner join jobs j
+	on e.job_id = j.job_id
+inner join locations l
+	on d.location_id = l.location_id
 
 
 
